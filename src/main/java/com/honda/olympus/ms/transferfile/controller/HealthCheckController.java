efa222278aed1bc5cac971c868a8643c6337fead
anttoniobh <abhernandez@stefanini.com>
2023-05-16 11:23:32 -0600

    First commit

---
+++ src/main/java/com/honda/olympus/ms/transferfile/controller/HealthCheckController.java
@@ -0,0 +1,34 @@
+package com.honda.olympus.ms.transferfile.controller;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HealthCheckController 
+{
+
+	@Value("${name}")
+	private String name;
+	
+	@Value("${version}")
+	private String version; 
+	
+	
+	@GetMapping("/health")
+	public ResponseEntity<String> healthCheck() 
+	{
+		String message = String.format("Honda Olympus [name: %s] [version: %s] %s %s", 
+			name, version, LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), TimeZone.getDefault().getID() );
+		
+		return new ResponseEntity<>(message, HttpStatus.OK);
+	}
+	
+}
