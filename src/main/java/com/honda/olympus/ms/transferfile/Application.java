8cd9cf2e4b14cda70da52dbc1d2440d242e1fddf
anttoniobh <abhernandez@stefanini.com>
2023-05-18 17:27:08 -0600

    Add messages and properties files

--- src/main/java/com/honda/olympus/ms/transferfile/Application.java
+++ src/main/java/com/honda/olympus/ms/transferfile/Application.java
@@ -6,9 +6,15 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 
 @SpringBootApplication
+@PropertySource(value = "classpath:properties.xml", ignoreResourceNotFound = false)
 public class Application 
 {
 
@@ -16,9 +22,27 @@ public class Application
 		SpringApplication.run(Application.class, args);
 	}
 	
+	
 	@PostConstruct
 	public void init() {
 		TimeZone.setDefault( TimeZone.getTimeZone("America/Mexico_City") );
+	}
+	
+	
+	@Bean
+	protected MessageSource messageSource() {
+	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+	    messageSource.setBasename("classpath:messages");
+	    messageSource.setDefaultEncoding("UTF-8");
+	    return messageSource;
+	}
+	
+	
+	@Bean
+	protected LocalValidatorFactoryBean getValidator() {
+	    LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+	    bean.setValidationMessageSource(messageSource());
+	    return bean;
 	}
 
 }
