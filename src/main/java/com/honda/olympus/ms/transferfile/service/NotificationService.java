75bc08ce3ff1731620eb769a2fcd477bc7a57232
anttoniobh <abhernandez@stefanini.com>
2023-05-19 01:42:25 -0600

    Add new service classes

---
+++ src/main/java/com/honda/olympus/ms/transferfile/service/NotificationService.java
@@ -0,0 +1,40 @@
+package com.honda.olympus.ms.transferfile.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+
+import com.honda.olympus.ms.transferfile.client.NotificationClient;
+import com.honda.olympus.ms.transferfile.domain.Event;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Service
+public class NotificationService 
+{
+	
+	@Autowired
+	private NotificationClient notificationClient;
+	
+	
+	public HttpStatus sendNotification(Event event) {
+		try {
+			ResponseEntity<String> response = this.notificationClient.sendNotification(event);
+			return response.getStatusCode();
+		}
+		catch (HttpClientErrorException exception) {
+			log.error("### Notification request error: {}", exception.getResponseBodyAsString());
+			return exception.getStatusCode();
+		}
+		catch (RestClientException exception) {
+			log.error("### Unable to connect to the Notification endpoint !", exception);
+			return null;
+		}
+	} 
+	
+}
