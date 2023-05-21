d5e6ca70b97dc7b19e03bd839397e30400ad4012
anttoniobh <abhernandez@stefanini.com>
2023-05-21 15:51:31 -0600

    Rename validation messages file

--- src/main/java/com/honda/olympus/ms/transferfile/Application.java
+++ src/main/java/com/honda/olympus/ms/transferfile/Application.java
@@ -32,7 +32,7 @@ public class Application
 	@Bean
 	protected MessageSource messageSource() {
 	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-	    messageSource.setBasename("classpath:messages");
+	    messageSource.setBasename("classpath:validation");
 	    messageSource.setDefaultEncoding("UTF-8");
 	    return messageSource;
 	}
