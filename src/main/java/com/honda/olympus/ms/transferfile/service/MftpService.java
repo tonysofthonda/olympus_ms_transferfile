0da549e1f6803938b9a07e817ab281b7a7d6d5a3
anttoniobh <abhernandez@stefanini.com>
2023-05-22 19:16:58 -0600

    Add new transferFile method to class MftpService

--- src/main/java/com/honda/olympus/ms/transferfile/service/MftpService.java
+++ src/main/java/com/honda/olympus/ms/transferfile/service/MftpService.java
@@ -3,15 +3,22 @@ package com.honda.olympus.ms.transferfile.service;
 import static java.lang.String.format;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.honda.olympus.ms.transferfile.client.MftpClient;
 import com.honda.olympus.ms.transferfile.client.MftpConfig;
 import com.honda.olympus.ms.transferfile.domain.Event;
 import com.honda.olympus.ms.transferfile.domain.Message;
+
+import lombok.Setter;
+
 import static com.honda.olympus.ms.transferfile.domain.Status.*;
 
 
+@Setter
 @Service
 public class MftpService 
 {
@@ -22,9 +29,6 @@ public class MftpService
 	private static final String MSG_INFO_ERROR = "El archivo %s no tiene información";
 	private static final String MSG_DOWNLOAD_ERROR = "Fallo al momento de realizar la copia del archivo %s, a la siguiente ubicación definida %s";
 	
-	private static final String MS_TRANSFERFILE = "ms.transferfile"; 
-	private static final String EMPTY = ""; 
-	
 	
 	@Autowired private MftpConfig config;
 	@Autowired private LogEventService logEventService;
@@ -32,24 +36,32 @@ public class MftpService
 	@Autowired private ProcessFileService processFileService;
 	
 	
-	public void transferFile(String fileName, String newFileName) 
-	{
+	@Value("${ms.transferfile.name}")
+	private String serviceName; 
+	
+	
+	public void transferFile(String fileName, String newFileName) {
 		MftpClient client = new MftpClient(config, fileName, newFileName);
-		
+		transferFile(client, fileName, newFileName);
+	}
+	
+	public void transferFile(MftpClient client, String fileName, String newFileName) 
+	{	
 		// connect to mftp server
 		if (!client.open()) {
 			Event event = connectionErrorEvent();
 			logEventService.logEvent(event);
 			notificationService.sendNotification(event);
-			return;
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, event.msg());
 		}
 		logEventService.logEvent( connectionOkEvent() );
 		
 		// search file
 		if (!client.fileExists()) {
 			client.close();
-			logEventService.logEvent( searchErrorEvent(fileName) );
-			return;
+			Event event = searchErrorEvent(fileName);
+			logEventService.logEvent(event);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, event.msg());
 		}
 		
 		// verify if empty
@@ -59,7 +71,7 @@ public class MftpService
 			Event event = infoErrorEvent(fileName);
 			logEventService.logEvent(event);
 			notificationService.sendNotification(event);
-			return;
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, event.msg());
 		}
 		
 		// download file
@@ -75,29 +87,29 @@ public class MftpService
 			Event event = downloadErrorEvent(fileName, newFileName);
 			logEventService.logEvent(event);
 			notificationService.sendNotification(event);
-			return;
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, event.msg());
 		}
 	}
 	
 	
 	private Event connectionOkEvent() {
-		return new Event(MS_TRANSFERFILE, _SUCCESS, format(MSG_CONNECTION_OK, config.host(), config.port()), EMPTY);
+		return new Event(serviceName, _SUCCESS, format(MSG_CONNECTION_OK, config.host(), config.port()), "");
 	}
 	
 	private Event connectionErrorEvent() {
-		return new Event(MS_TRANSFERFILE, _FAIL, format(MSG_CONNECTION_ERROR, config.host(), config.port()), EMPTY);
+		return new Event(serviceName, _FAIL, format(MSG_CONNECTION_ERROR, config.host(), config.port()), "");
 	}
 	
 	private Event searchErrorEvent(String fileName) {
-		return new Event(MS_TRANSFERFILE, _FAIL, format(MSG_SEARCH_ERROR, fileName, config.inbound()), fileName);
+		return new Event(serviceName, _FAIL, format(MSG_SEARCH_ERROR, fileName, config.inbound()), fileName);
 	}
 	
 	private Event infoErrorEvent(String fileName) {
-		return new Event(MS_TRANSFERFILE, _FAIL, format(MSG_INFO_ERROR, fileName), fileName);
+		return new Event(serviceName, _FAIL, format(MSG_INFO_ERROR, fileName), fileName);
 	}
 	
 	private Event downloadOkEvent(String newFileName) {
-		return new Event(MS_TRANSFERFILE, _SUCCESS, SUCCESS, newFileName);
+		return new Event(serviceName, _SUCCESS, SUCCESS, newFileName);
 	}
 	
 	private Message downloadOkMessage(String newFileName) {
@@ -105,11 +117,7 @@ public class MftpService
 	}
 	
 	private Event downloadErrorEvent(String fileName, String newFileName) {
-		return new Event(MS_TRANSFERFILE, _FAIL, format(MSG_DOWNLOAD_ERROR, newFileName, config.destination()), fileName);
-	}
-	
-	public Event errorEvent(String msg) {
-		return new Event(MS_TRANSFERFILE, _FAIL, msg, EMPTY);
+		return new Event(serviceName, _FAIL, format(MSG_DOWNLOAD_ERROR, newFileName, config.destination()), fileName);
 	}
 	
 }
