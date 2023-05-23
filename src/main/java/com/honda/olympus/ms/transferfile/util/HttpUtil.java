9bb304e326ef8e28c2cd800004db415f26369039
anttoniobh <abhernandez@stefanini.com>
2023-05-22 20:36:33 -0600

    Update property files

--- src/main/java/com/honda/olympus/ms/transferfile/util/HttpUtil.java
+++ src/main/java/com/honda/olympus/ms/transferfile/util/HttpUtil.java
@@ -7,27 +7,17 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.web.server.ResponseStatusException;
 
-import lombok.extern.slf4j.Slf4j;
 
-
-@Slf4j
 public final class HttpUtil 
 {	
 	
 	private HttpUtil() { }
-	
 	
 	public static void handleBadRequest(Errors errors) {
 		if (errors.hasErrors()) {
 			String messages = errors.getFieldErrors().stream().map(FieldError::getDefaultMessage).collect(joining("; "));
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, messages);
 		}
-	}
-	
-	
-	public static void handleBadResponse(String message, Exception exception) {
-		log.error("### {}", message, exception);
-		throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, message);
 	}
 	
 }
