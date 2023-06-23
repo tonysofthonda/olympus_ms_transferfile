8a2f982a2178c0e41f00d51d3ffda567644dc337
vzpintor <ovpintor@stefanini.com>
2023-06-23 11:44:07 -0600

    fix health service to show active profile

--- src/main/java/com/honda/olympus/ms/transferfile/controller/HealthCheckController.java
+++ src/main/java/com/honda/olympus/ms/transferfile/controller/HealthCheckController.java
@@ -2,9 +2,12 @@ package com.honda.olympus.ms.transferfile.controller;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.TimeZone;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,23 +17,24 @@ import io.swagger.v3.oas.annotations.Operation;
 
 
 @Controller
-public class HealthCheckController 
-{
+public class HealthCheckController {
 
-	@Value("${service.name}")
-	private String name;
-	
-	@Value("${service.version}")
-	private String version; 
-	
-	@Operation(summary = "Test service availability and version")
-	@GetMapping("/health")
-	public ResponseEntity<String> healthCheck() 
-	{
-		String message = String.format("Honda Olympus [name: %s] [version: %s] %s %s", 
-			name, version, LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), TimeZone.getDefault().getID() );
-		
-		return new ResponseEntity<>(message, HttpStatus.OK);
-	}
-	
+    @Autowired
+    private Environment environment;
+
+    @Value("${service.name}")
+    private String name;
+
+    @Value("${service.version}")
+    private String version;
+
+    @Operation(summary = "Test service availability and version")
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+		String message = String.format("Honda Olympus [name: %s] [version: %s] [profile: %s] %s %s",
+				name, version, Arrays.toString(environment.getActiveProfiles()), LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), TimeZone.getDefault().getID() );
+
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
 }
